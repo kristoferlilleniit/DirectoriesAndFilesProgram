9da89f7c11c4b1d92a423470a0789ca37fcf24c9
Opilane TTHK <opilane@tthk.ee>
2020-11-12 13:21:01 +0200

    first commit

---
+++ DirectoriesAndFilesProgram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoriesAndFilesProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootpath = @"C:\Users\opilane\Samples";
+
+            string[] allDirectories = Directory.GetDirectories(rootpath);
+            ReadArray(allDirectories);
+
+            string[] includeNestedDirectories = Directory.GetDirectories(rootpath, "*.*", SearchOption.AllDirectories);
+            ReadArray(includeNestedDirectories);
+
+            string[] files = Directory.GetFiles(rootpath, "*.*", SearchOption.AllDirectories);
+            ReadArray(files);
+        }
+
+        public static void ReadArray(string[] array)
+        {
+            foreach(string line in array)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ WriteFilesAndDirectories/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace WriteFilesAndDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteFilePath();
+        }
+
+        public static void WriteFilePath()
+        {
+            string filepath = @"C:\Users\opilane\Samples\files.txt";
+            string directoryPath = @"C:\Users\opilane\Samples";
+            string rootpath = @"C:\Users\opilane\Samples";
+            string folderfilepath = @"C:\Users\opilane\Samples\directories.txt";
+
+            string[] files = Directory.GetFiles(directoryPath, "*.*", SearchOption.AllDirectories);
+
+            File.WriteAllLines(filepath, files);
+
+            string[] allDirectories = Directory.GetDirectories(rootpath);
+            File.WriteAllLines(folderfilepath, allDirectories);
+
+            string[] includeNestedDirectories = Directory.GetDirectories(rootpath, "*.*", SearchOption.AllDirectories);
+            File.WriteAllLines(folderfilepath, includeNestedDirectories);
+
+        }
+
+    }
+}
